78d012b53c0cd5b6c0b0a5c1f040fbd70bc17736
ANUSHALAV <bhattanushalav@gmail.com>
2026-02-14 17:45:04 +0530

    implement header and notes list components with styling and tests

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { NotesList } from './layout/notes-list/notes-list';
+import { Component } from '@angular/core';
+import { Header } from './layout/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header,NotesList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Notely');
 }
---
+++ src/app/layout/notes-list/notes-list.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notes-list',
+  imports: [],
+  templateUrl: './notes-list.html',
+  styleUrl: './notes-list.css',
+})
+export class NotesList {
+
+  onAddNote(){
+    
+  }
+}
